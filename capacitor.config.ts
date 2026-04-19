e28feb2aeafa93731f71a3b6877b113878b06ab5
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2026-04-19 12:50:49 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'bo.edu.proyecto.orientacion',
+  appName: 'sistema-orientacion-aeroportuaria',
+  webDir: 'www'
+};
+
+export default config;
